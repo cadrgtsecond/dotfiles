b17ef7c07e2216a2aa9e93fa44bd222e182bf253
Abhinav Krishna <abhinavkrishnacr2020@gmail.com>
2024-12-22 19:52:55 +0530

    Added kakoune

--- setup.sh
+++ setup.sh
@@ -5,3 +5,4 @@ ln -sf $PWD/bashrc ~/.bashrc
 ln -sf $PWD/config/river ~/.config/river
 ln -sf $PWD/config/foot ~/.config/foot
 ln -sf $PWD/config/yambar ~/.config/yambar
+ln -sf $PWD/config/kak ~/.config/kak
