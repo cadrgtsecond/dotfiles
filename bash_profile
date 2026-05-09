cd58cfc8eafcfd6d3d8d7422add20006b004339a
cadr  second <abhinavkrishnacr2020@gmail.com>
2026-05-09 12:35:37 +0530

    update dots

--- bash_profile
+++ bash_profile
@@ -2,10 +2,10 @@
 source /etc/profile
 
 HISTFILE=''
-export PATH=$PATH:~/.local/bin:~/.local/bin/scripts
+export PATH=~/.local/bin:~/.local/bin/scripts:$PATH
 export RUSTFLAGS="-C target-feature=-crt-static"
 export EDITOR=kak
-export BROWSER=brw
+export BROWSER=vimb
 export BEMENU_OPTS="-W 0.8 --fn /usr/share/fonts/iosevka/Iosevka-Regular.ttc"
 export FZF_DEFAULT_COMMAND='fd -t f'
 export FZF_DEFAULT_OPTS='--layout=reverse -m'
--- setup.sh
+++ setup.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
 # We need to use $PWD because symbolic links are glorified strings
-ln -sf "$PWD/scripts" ~/.local/bin/scripts
-ln -sf "$PWD/bashrc" ~/.bashrc
-ln -sf "$PWD/bash_profile" ~/.bash_profile
+ln -sfT "$PWD/scripts" ~/.local/bin/scripts
+ln -sfT "$PWD/bashrc" ~/.bashrc
+ln -sfT "$PWD/bash_profile" ~/.bash_profile
 
 for f in "$PWD/config/"*; do
     ln -sf "$f" ~/.config/
 done
+ln -sfT "$PWD/themes" ~/.config/themes
 
 # Don't change preexisting services
 [ -d ~/.local/share ] && exit
